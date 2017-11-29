b7a88f6501db7414e0fb45757194721cc14fc3d1
Realiza <realiza@Realiza-7.local>
2017-11-29 17:00:02 -0200

    Adicionei informacoes novas

--- jsTCC/TCC.js
+++ jsTCC/TCC.js
@@ -41,4 +41,47 @@ $("#imgP").click(function(){
   $(".apartamento").hide();
   $("#PGP").show();
 });
+
+
+$(".btInfTe").click(function(){
+  
+  var button = $(this);
+  var div = $(button.parent()[0]);
+  var text = div.children('.texTerre');
+
+  text.show();
+
+
+  $(".btInfSo").click(function(){
+    
+    var button = $(this);
+    var div = $(button.parent()[0]);
+    var text = div.children('.textSobra');
+    
+   
+  text.show();
+    
+  $(".btInfCasa").click(function(){
+      
+    var button = $(this);
+    var div = $(button.parent()[0]);
+    var text = div.children('.textCasa');
+      
+  text.show();
+    
+  $(".btInfApart").click(function(){
+    
+    var button = $(this);
+    var div = $(button.parent()[0]);
+    var text = div.children('.textApart');
+    
+  text.show();
+  
+    
+    }); 
+   }); 
+  }); 
+});
+
+
 });
