83edbf5d6a1c4e463a50e4458bf75df113ea7fa0
Realiza <realiza@Realiza-7.local>
2017-12-04 16:59:28 -0200

    modifiquei algumas coisas

--- jsTCC/TCC.js
+++ jsTCC/TCC.js
@@ -48,40 +48,92 @@ $(".btInfTe").click(function(){
   var button = $(this);
   var div = $(button.parent()[0]);
   var text = div.children('.texTerre');
-
+  var backButton = div.children('.btSinFte');
+  
   text.show();
-
+  backButton.show();
+});
 
   $(".btInfSo").click(function(){
     
     var button = $(this);
     var div = $(button.parent()[0]);
     var text = div.children('.textSobra');
-    
+    var backButton = div.children('.btSinFsO');
    
   text.show();
-    
+  backButton.show();  
+  });
+
   $(".btInfCasa").click(function(){
       
     var button = $(this);
     var div = $(button.parent()[0]);
     var text = div.children('.textCasa');
-      
+    var backButton = div.children('.btSinFcA');  
+  
   text.show();
-    
+  backButton.show(); 
+  });
+
   $(".btInfApart").click(function(){
     
     var button = $(this);
     var div = $(button.parent()[0]);
     var text = div.children('.textApart');
+    var backButton = div.children('.btSinFaP');
     
+
+
   text.show();
-  
+  backButton.show();
+
+  });
+
+  $(".btSinFte").click(function(){
     
+    var button = $(this);
+    var div = $(button.parent()[0]);
+    var text = div.children('.texTerre');
+    var backButton = div.children('.btSinFte');
+    
+    text.hide();
+    backButton.hide();
+  
+  
+     });  
+
+     $(".btSinFsO").click(function(){
+      
+      var button = $(this);
+      var div = $(button.parent()[0]);
+      var text = div.children('.textSobra');
+      var backButton = div.children('.btSinFsO');
+     
+    text.hide();
+    backButton.hide();  
+     });
+
+     $(".btSinFcA").click(function(){
+      
+    var button = $(this);
+    var div = $(button.parent()[0]);
+    var text = div.children('.textCasa');
+    var backButton = div.children('.btSinFcA');   
+ 
+    text.hide();
+    backButton.hide();
+     });
+
+
+     $(".btSinFaP").click(function(){
+      
+      var button = $(this);
+      var div = $(button.parent()[0]);
+      var text = div.children('.textApart');
+      var backButton = div.children('.btSinFaP');
+    
+    text.hide();
+    backButton.hide();
+     });
     }); 
-   }); 
-  }); 
-});
-
-
-});
